ff069bea3a7b10f30b9344fb74ef6a2ff976dbf6
Chris <christina.friede5@gmail.com>
2020-09-03 18:12:17 +0200

    Add printing Log

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function(){
             let damage = this.calculateDamage(3, 10);
@@ -25,7 +26,12 @@ new Vue({
             this.monsterAttacks();
         },
         specialAttack: function(){
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            let damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
             if(this.checkWin()){
                 return;
             }
@@ -37,6 +43,10 @@ new Vue({
             } else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
             this.monsterAttacks();
         },
         giveUp: function(){
@@ -45,11 +55,11 @@ new Vue({
         monsterAttacks: function() {
             let damage = this.calculateDamage(5, 12);
             this.playerHealth -= damage;
+            this.checkWin();
             this.turns.unshift({
                 isPlayer: false,
                 text: 'Monster hits Player for ' + damage
             });
-            this.checkWin();
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
