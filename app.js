29ea5a2c7bfb29a5ed4aae28351700977ee99413
Chris <christina.friede5@gmail.com>
2020-09-03 17:04:12 +0200

    Add checkWin function

--- app.js
+++ app.js
@@ -13,19 +13,12 @@ new Vue({
         },
         attack: function(){
             this.monsterHealth -= this.calculateDamage(3, 10);
-
-            if (this.monsterHealth <= 0) {
-                alert('You won!');
-                this.gameIsRunning = false;
+            if(this.checkWin()){
                 return;
             }
 
             this.playerHealth -= this.calculateDamage(5, 12);
-
-            if (this.playerHealth <= 0) {
-                alert('You lost!');
-                this.gameIsRunning = false;
-            }
+            this.checkWin();
         },
         specialAttack: function(){
 
@@ -38,6 +31,24 @@ new Vue({
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                if(confirm('You won! New game?')){
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                } 
+                return true;
+            } else if(this.playerHealth <= 0){
+                if(confirm('You lost! New game?')){
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                } 
+                return true;
+            }
+            return false;
         }
     }
 });
